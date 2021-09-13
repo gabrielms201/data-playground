88843c484f0e41eff92f89984b117c2d8c6a80bf
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-09-13 15:10:54 -0300

    Add option to track milliseconds

--- data-manager/data-collector.py
+++ data-manager/data-collector.py
@@ -15,11 +15,15 @@ class Metric():
     # Content getter && setter
     def setContent(self, data, line):
         if self.type == "NUMERIC":
-            data = data[line][self.column].replace(",", ".").replace(" ", "0")
+            data = data[line][self.column].replace(",", ".").replace(" ", "0").replace('"', "")
             return self.__content.append(float(data))
         elif self.type == "DATE":
-            time = data[line][self.column]
-            return self.__content.append(dt.datetime.strptime(time, "%d/%m/%Y %H:%M"))
+            time = data[line][self.column].replace('"', "")
+            dateFormat = "%m/%d/%Y %H:%M"
+            if len(time) <= 16:
+                return self.__content.append(dt.datetime.strptime(time, dateFormat))
+            else:
+                return self.__content.append(dt.datetime.strptime(time, dateFormat + ":%S.%f"))
     def getContent(self):
         return self.__content
     # Name getter
@@ -37,6 +41,7 @@ class DataBase():
         data = [words.split("\t") for words in data]
         file.close()
         self.__data = data
+        self.fileName = filename
     def getData(self):
         return self.__data
     # Metrics getter
@@ -52,7 +57,7 @@ class Graphic:
     def __init__(self, data):
         self.__data = data
     
-    def viewGraphicData(self, x, y, shoudPlot = False, scatterColor = "blue", scatterSize = 5, linewidth = 2, plotColor = "red"):
+    def viewGraphicData(self, x, y, shoudPlot = False, scatterColor = "blue", scatterSize = 5, linewidth = 1, plotColor = "red"):
         xContent = x.getContent()
         yContent = y.getContent()
 
@@ -60,15 +65,19 @@ class Graphic:
         ax.set_xticks(xContent)
         mpl.rcParams["figure.dpi"] = 150
         ax.xaxis.set_major_formatter(md.DateFormatter("%H:%M"))
-        plt.xticks(rotation=90)
+        plt.xticks(rotation = 90, size = 8)
+        plt.locator_params(axis="x", nbins=(len(xContent)/3))
+        plt.locator_params(axis="y", nbins=(len(yContent)/10))
 
         plt.scatter(xContent, yContent, color = scatterColor, s = scatterSize)
         if shoudPlot: 
             plt.plot(xContent, yContent, color = plotColor, linewidth = linewidth)
         
         # Labels && Legends
+        plt.title(self.__data.fileName)
         plt.ylabel(y.getName())
         plt.xlabel(x.getName())
+        plt.grid(alpha = 0.5)
         plt.show()
 
 def main():
@@ -76,13 +85,14 @@ def main():
     time = Metric("Time (HH:MM)", "Date", 0)
     freeRam = Metric("Ram Free (MB)", "Numeric", 1)
     cpu = Metric("Cpu Usage (%)", "Numeric", 24)
-    commited = Metric("RAM Commited (%)", "Numeric", 25)
-    metrics = [time, freeRam, cpu, commited]
-    # DataBase -> Idea: Use xml to define .tsv path
+    commitedRam = Metric("RAM Commited (%)", "Numeric", 25)
+    metrics = [time, freeRam, cpu, commitedRam]
+    # DataBase -> Idea: Use xml to define .tsv path and filename
     dataBase = DataBase(metrics)
-    dataBase.setData("collector-input/Data.tsv")
+    file = "collector-input/data-10-09.tsv"
+    dataBase.setData(file)
     dataBase.fillMetrics()
     # Graphi -> Idea: Use xml or a gui to define what metrics should view (i think it'be better a gui)
     graphic = Graphic(dataBase)
-    graphic.viewGraphicData(time, cpu, shoudPlot = True, scatterColor = "red", scatterSize = 10, linewidth = 2, plotColor = "blue")
+    graphic.viewGraphicData(time, freeRam, shoudPlot = True, scatterColor = "red", scatterSize = 10, linewidth = 1, plotColor = "blue")
 main()
