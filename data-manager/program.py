299c337326978f0ca36b844fa1f133521ce275f7
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-09-12 22:42:16 -0300

    Add Metrics attribute for the class "DataBase"

--- data-manager/program.py
+++ data-manager/program.py
@@ -7,7 +7,7 @@ class Metric():
         self.type = type.upper()
         self.column = column
         self.__content = []
-    # --> 
+    # Content getter && setter
     def setContent(self, data, line):
         if self.type == "NUMERIC":
             return self.__content.append(data[line][self.column].replace(",", "."))
@@ -17,9 +17,10 @@ class Metric():
         return self.__content
     # -->   
 class DataBase():
-    def __init__(self):
-        pass
+    def __init__(self, metrics):
+        self.__metrics = metrics
     # --> 
+    # Data getter && setter
     def setData(self, filename):
         file = open(filename, "r", encoding="UTF-8")
         data = [line.rstrip("\n") for line in file]
@@ -28,21 +29,25 @@ class DataBase():
         self.__data = data
     def getData(self):
         return self.__data
-    # --> 
-    def fillMetrics(self, metrics):
-        for metric in metrics:
+    # Metrics getter
+    def getMetrics(self, metrics):
+        return self.__metrics
+    # Custom methods
+    def fillMetrics(self):
+        for metric in self.__metrics:
             for line in range(1, len(self.__data)):
                 metric.setContent(self.__data, line)
 
 
 def main():
-    # Db
-    dataBase = DataBase()
-    dataBase.setData("Data.tsv")
     # Metrics
     time = Metric("Date", 0)
     free = Metric("Numeric", 1)
     cpu = Metric("Numeric", -2)
     commited = Metric("Numeric", -1)
-
-    dataBase.fillMetrics([time, free, cpu, commited])
+    # Db
+    dataBaseMetrics = [time, free, cpu, commited]
+    dataBase = DataBase(dataBaseMetrics)
+    dataBase.setData("Data.tsv")
+    dataBase.fillMetrics()
+main()
