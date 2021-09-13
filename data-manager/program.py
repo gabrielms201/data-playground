200acf4cde394b2ee03cf2e0e2748a8c42cb6be9
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-09-13 11:11:37 -0300

    Add some graphic view

--- data-manager/program.py
+++ data-manager/program.py
@@ -1,4 +1,6 @@
+import datetime as dt
 import numpy as np
+import matplotlib.dates as md
 import matplotlib.pyplot as plt
 
 
@@ -10,16 +12,15 @@ class Metric():
     # Content getter && setter
     def setContent(self, data, line):
         if self.type == "NUMERIC":
-            return self.__content.append(data[line][self.column].replace(",", "."))
+            return self.__content.append( float (data[line][self.column].replace(",", ".").replace(" ", "0")) )
         elif self.type == "DATE":
-            return self.__content.append(data[line][self.column].replace("09/09/2021", "").strip())
+            time = data[line][self.column]
+            return self.__content.append(dt.datetime.strptime(time, "%d/%m/%Y %H:%M"))
     def getContent(self):
         return self.__content
-    # -->   
 class DataBase():
     def __init__(self, metrics):
         self.__metrics = metrics
-    # --> 
     # Data getter && setter
     def setData(self, filename):
         file = open(filename, "r", encoding="UTF-8")
@@ -38,7 +39,6 @@ class DataBase():
             for line in range(1, len(self.__data)):
                 metric.setContent(self.__data, line)
 
-
 def main():
     # Metrics
     time = Metric("Date", 0)
@@ -50,4 +50,16 @@ def main():
     dataBase = DataBase(dataBaseMetrics)
     dataBase.setData("Data.tsv")
     dataBase.fillMetrics()
+
+    # Graphic
+    x = time.getContent()
+    y = free.getContent()
+    del x[89:] ; del y[89:]
+    ax=plt.gca()
+    ax.set_xticks(x)
+    ax.xaxis.set_major_formatter(md.DateFormatter("%H:%M"))
+    plt.xticks(rotation=90)
+    plt.subplots_adjust(bottom=0.5)
+    plt.scatter(x,y)
+    plt.show()
 main()
